4e0b2761b5f5b08dcf42f8d17ee41065b2af5ee2
Naomi-Stringer <n.stringer@unsw.edu.au>
2017-09-18 14:21:10 +1000

    Made a battery object

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ class Network:
             total += p.calc_net_export(date_time, interval_min)
         return total
 
-
 mynetwork = Network('Byron')
 mynetwork.test()
 
@@ -40,7 +39,30 @@ class Participant:
     def calc_net_export(self, date_time, interval_min):
         return np.random.uniform(0,10) 
 
-    
+class Central_Battery:
+    def __init__(self, cap_kWh, cap_kW, cycle_eff):
+        """Make note: cycle efficiency must be between zero and one."""
+        self.cap_kWh = cap_kWh
+        self.cap_kW = cap_kW
+        self.cycle_eff = cycle_eff
+        self.charge_level_kWh = 0  
+        self.num_cycles = 0      
+
+    def charge(self, kWh):
+        # Increase battery charge level by the input kWh
+        amount_to_charge = min(self.cap_kWh - self.charge_level_kWh, kWh)
+        self.charge_level_kWh += amount_to_charge * self.cycle_eff
+        return kWh - amount_to_charge
+
+    def discharge(self, kWh_request):
+        discharge_amount = min(kWh_request, self.charge_level_kWh)
+        self.charge_level_kWh -= discharge_amount
+        self.num_cycles += float(discharge_amount) / float(self.cap_kWh)
+        return discharge_amount
+
+    def get_num_cycles(self):
+        return self.num_cycles
+
 
 participant_1 = Participant('solar', 'A', 'ENOVA')
 participant_2 = Participant('load', 'B', 'ENOVA')
